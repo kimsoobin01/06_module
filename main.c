6d48a18ddd421ac29169baf2a380d208b17ff5e7
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-10-10 16:02:16 +0900

    sumTwo

--- main.c
+++ main.c
@@ -3,16 +3,37 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void print_star()
+int sumTwo( int a, int b)
 {
-	int i;
-	
-	for(i=0; i<10; i++)
-		printf("*");
+	int output;
+	output = a+b;
+	return output; //output; ok
 }
-int main(void)
+
+int square (int n)
 {
-	print_star();
-	print_star();
-	print_star();
+	return (n*n);
+}
+
+int get_max(int a, int b)
+{
+	if (a>b) return a;
+		
+	else return b;
+}
+
+
+int main(){
+	
+	int x, y;
+	int output;
+	
+	x=2;
+	y=5;
+	
+	output = sumTwo(x, y);
+	printf("sumTwo : %i\n", output);
+	printf("square : %i\n", square(x));
+	
+	return 0;
 }
