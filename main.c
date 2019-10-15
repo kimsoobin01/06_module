ca1306898589ad626bc8f012b369a1aed53c19d7
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-10-15 16:22:12 +0900

    return

--- main.c
+++ main.c
@@ -3,39 +3,17 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int factorial(int n)
-{
-	int res = 1;
-	int i;
-	for (i=1; i <= n ; i++);
-	{
-		res = res*i;
-	}
-	return res;
-}
+int square(int n);
 
-int calccombination (int n, int r)
+int main(void)
 {
-	int high, low;
-	high = factorial(n);
-	low = factorial(n-r)*factorial(r);
-	
-	return (high/low);
+	int result;
+	result = square(5);
+	printf("%d", result);
 }
-
-int main(int argc, char *argv[])
+int square(int n)
 {
-	int n;
-	int r;
-	int res;
-	
-	printf("input n and r");
-	scanf("%d %d", &n, &r);
-	
-	res = calccombination(n, r);
-	printf("combination result is %d\n", res);
-	
-	return 0;
+	return (n*n);
 }
 
 
